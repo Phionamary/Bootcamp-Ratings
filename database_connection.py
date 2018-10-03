ea5ba0794e597374832b8dbbc6e510b1e64591fc
Opio Emmanuel Omona <emmanuelomona1@gmail.com>
2018-10-03 12:12:45 +0300

    signup

--- database_connection.py
+++ database_connection.py
@@ -43,6 +43,28 @@ class DatabaseConnection():
        self.connection.commit()
        print("Score table created")
 
+    def signup(self, user_dict):
+    	sql = (
+    		'''
+    		INSERT INTO "User"(user_id, username, email, password, role)
+    		VALUES(DEFAULT, %s, %s, %s, %s) RETURNING user_id, username, email, password, role;
+    		'''
+    	)
+    	self.cursor.execute(sql, [user_dict['username'], user_dict['email'], user_dict['password'], user_dict['role']])
+    	self.connection.commit()
+        print("User added")
+
+    def rate_user(self, score_dict):
+    	sql = (
+    		'''
+    		INSERT INTO "Score"(user_id, excellence, passion, integrity, collaboration)
+    		VALUES(%s, %s, %s, %s, %s) RETURNING user_id, excellence, passion, integrity, collaboration;
+    		'''
+    	)
+    	self.cursor.execute(sql, [score_dict['user_id'], score_dict['excellence'], score_dict['passion'], score_dict['integrity'], score_dict['collaboration']])
+    	self.connection.commit()
+    	print ("score updated")
+
 user = DatabaseConnection("maria")
 user.create_user_table()
 user.create_scores_table()
